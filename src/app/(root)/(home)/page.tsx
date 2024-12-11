b41f69e3ccdcb37f9748fce61e74bd96a9851e0a
Olovuddin <olovuddinramiddinov26@gmail.com>
2024-12-11 11:57:55 +0500

    Auth Page

---
+++ src/app/(root)/(home)/page.tsx
@@ -0,0 +1,13 @@
+import Auth from "@/components/auth";
+import React from "react";
+
+export default function Page() {
+  const user = false;
+  if (!user)
+    return (
+      <div className="container h-screen mx-auto max-w-7xl px-4 md:px-0">
+        <Auth />
+      </div>
+    );
+  return <div>Page</div>;
+}
